9e3a3d845bb3e3e09046f9732ed0c3cc8b0cd157
Keith Minkler <kminkler@gmail.com>
2016-06-05 21:03:32 +0000

    initial configuration

--- x2engine/protected/config/X2Config.php
+++ x2engine/protected/config/X2Config.php
@@ -1,12 +1,12 @@
 <?php
-$appName = 'X2Engine';
-$email = 'a@b.com';
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'x2engine';
+$appName = 'X2CRM';
+$email = 'kminkler@gmail.com';
+$host = 'mlsdata.c7amitgq5cmw.us-east-1.rds.amazonaws.com';
+$user = 'mlsdata01';
+$pass = 'mlsdata01';
+$dbname = 'chmbuffalo_crm';
 $version = '6.0';
-$buildDate = '1461528248';
+$buildDate = 1461528248;
 $updaterVersion = '6.0';
 $language='en';
 ?>
